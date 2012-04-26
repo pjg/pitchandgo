142a02f9243ae88215a2056189bdf9de7951b900
Paweł Gościcki <pawel.goscicki@gmail.com>
2012-04-27 00:38:47 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PitchAndGo::Application.config.secret_token = '0b283ded6f57ed09094e4825c29b97271a7c4f3034040634b42255f68d051f1f4b82eac2c8e2da6cf02922aeb3a2c8e6e0c3fa03a6137b1810b4ad1b0375e246'
