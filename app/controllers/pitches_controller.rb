9b1084c6f7e71313656a7c72d527e086f3874a6a
Jakub Naliwajek <naliwajek@gmail.com>
2012-04-28 17:55:00 +0200

    user model

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -4,4 +4,14 @@ class PitchesController < ApplicationController
   def index
     respond_with Pitch.order(:id)
   end
+
+  def vote
+    if current_user.votes.count < 3
+      current_user.votes.create(:pitch_id => params[:id])
+      render :json => { :status => "success" }
+    else
+      render :json => { :status => "failure" }
+    end
+  end
+
 end
--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,3 +1,4 @@
 class Pitch < ActiveRecord::Base
   attr_accessible :description, :name, :user_id
+  has_many :votes
 end
